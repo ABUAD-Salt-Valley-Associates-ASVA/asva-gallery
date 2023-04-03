a2f5a89c624fa80b0f04834875278c3df974310e
James Onyekachi <29162889+okilonet1@users.noreply.github.com>
2023-04-03 17:52:41 +0100

    Modified og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,25 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="See pictures from ASVA" />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta property="og:description" content="See pictures from ASVA" />
-          <meta property="og:title" content="ASVA Gallery" />
+          <meta
+            name="description"
+            content="Step into a world of nostalgia with ABUAD Salt Valley Associates' breathtaking photo gallery. Immerse yourself in a visual journey through time and relive unforgettable moments captured by our skilled photographers. Join us as we celebrate the beauty and essence of our community through the power of photography."
+          />
+          <meta property="og:site_name" content="gallery.asva.tech" />
+          <meta
+            property="og:description"
+            content="Step into a world of nostalgia with ABUAD Salt Valley Associates' breathtaking photo gallery. Immerse yourself in a visual journey through time and relive unforgettable moments captured by our skilled photographers. Join us as we celebrate the beauty and essence of our community through the power of photography"
+          />
+          <meta
+            property="og:title"
+            content="Relive Past Memories with ABUAD Salt Valley Associates (ASVA)"
+          />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="ASVA Gallery" />
-          <meta name="twitter:description" content="See pictures from ASVA" />
+          <meta name="twitter:title" content="ASVA Photo Gallery" />
+          <meta
+            name="twitter:description"
+            content="Step into a world of nostalgia with ABUAD Salt Valley Associates' breathtaking photo gallery. Immerse yourself in a visual journey through time and relive unforgettable moments captured by our skilled photographers. Join us as we celebrate the beauty and essence of our community through the power of photography"
+          />
         </Head>
         <body className="bg-black antialiased">
           <Main />
